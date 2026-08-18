48cb0182733a179c07f6f8c3e75654c9ae1f41c8
pulsathost-png <pulsathost@gmail.com>
2026-08-18 11:48:14 +0300

    Update servers.py

--- services/servers.py
+++ services/servers.py
@@ -1,59 +1,38 @@
-from database import connect
+from database import (
+    add_server,
+    get_servers,
+    update_server_status
+)
 
 
-def create_server(user_id, name):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        """
-        INSERT INTO servers (user_id, name, status)
-        VALUES (?, ?, ?)
-        """,
-        (user_id, name, "OFF")
+def create_server(user_id, name, plan):
+    add_server(
+        user_id,
+        name,
+        plan
     )
 
-    conn.commit()
-    conn.close()
 
-
-def get_servers(user_id):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "SELECT id, name, status FROM servers WHERE user_id=?",
-        (user_id,)
-    )
-
-    servers = cursor.fetchall()
-
-    conn.close()
-
-    return servers
+def user_servers(user_id):
+    return get_servers(user_id)
 
 
 def start_server(server_id):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "UPDATE servers SET status=? WHERE id=?",
-        ("ON", server_id)
+    update_server_status(
+        server_id,
+        "ON"
     )
-
-    conn.commit()
-    conn.close()
 
 
 def stop_server(server_id):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "UPDATE servers SET status=? WHERE id=?",
-        ("OFF", server_id)
+    update_server_status(
+        server_id,
+        "OFF"
     )
 
-    conn.commit()
-    conn.close()
+
+def restart_server(server_id):
+    update_server_status(
+        server_id,
+        "RESTART"
+    )
